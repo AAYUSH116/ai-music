af87e3aa59f89465abb2a6a58ec674a0cff444f8
AAYUSH116 <82816625+AAYUSH116@users.noreply.github.com>
2021-06-15 18:17:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1="";
 song2="";
+leftwristX=0;
+leftwristY=0;
+rightwristX=0;
+rightwristY=0;
 
 function preLoad(){
     song1=loadSound(song1.mp3);
@@ -12,8 +16,22 @@ function setup(){
 
    video=createCapture(VIDEO);
    video.hide();
+
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPose);
+}
+
+function modelLoaded(){
+    console.log('PoseNet is initialized');
 }
 
 function draw(){
     image(video,0,0,600,600);
+}
+
+function gotPose(results){
+if(results.length>0){
+leftwristX=results[0].pose.leftwrist.x;
+leftwristY=results[0].pose.leftwrist.x;
+}
 }
